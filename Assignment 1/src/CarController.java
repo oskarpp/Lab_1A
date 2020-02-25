57d8cdc97bc9b1733aa4808766d93abbf10c0a12
04320 <alexey.ryabov@hotmail.com>
2020-02-25 08:27:36 +0100

    Update CarController.java
    test kom.

--- Assignment 1/src/CarController.java
+++ Assignment 1/src/CarController.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 * It's responsibilities is to listen to the View and responds in a appropriate manner by
 * modifying the model state and the updating the view.
  */
-
+ // TEST kommentar!
 public class CarController {
     // member fields:
 
